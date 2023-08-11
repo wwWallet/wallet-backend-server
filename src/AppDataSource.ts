2b78e069e9fe5ff335bd09ec4930ba696416fe5c
Emil Lundberg <emil@yubico.com>
2023-08-11 17:32:36 +0200

    Fix indentation in AppDataSource.ts

--- src/AppDataSource.ts
+++ src/AppDataSource.ts
@@ -4,42 +4,42 @@ import config from "../config";
 
 // Initialize DB connection
 const AppDataSource: DataSource = new DataSource({
-    type: "mysql",
-    host: config.db.host,
-    port: config.db.port,
-    username: config.db.username,
-    password: config.db.password,
-    database: config.db.dbname,
-    entities: [__dirname + "/entities/*.entity.{js,ts}"],
-    synchronize: true
+	type: "mysql",
+	host: config.db.host,
+	port: config.db.port,
+	username: config.db.username,
+	password: config.db.password,
+	database: config.db.dbname,
+	entities: [__dirname + "/entities/*.entity.{js,ts}"],
+	synchronize: true,
 });
 
 (async function initDataSource() {
-  let connected = false;
+	let connected = false;
 
-  console.log("Connecting with DB...");
-  await AppDataSource.initialize()
-    .then(() => {
-      console.log("App Data Source has been initialized!");
-      connected = true;
-    })
-    .catch((err) => {
-      console.error("Error during Data Source initialization", err);
-    });
+	console.log("Connecting with DB...");
+	await AppDataSource.initialize()
+		.then(() => {
+			console.log("App Data Source has been initialized!");
+			connected = true;
+		})
+		.catch((err) => {
+			console.error("Error during Data Source initialization", err);
+		});
 
-  // if not connected, then retry in loop
-  while (!connected) {
-    await new Promise((resolve) => setTimeout(resolve, 3000)).then(async () => {
-      await AppDataSource.initialize()
-        .then(() => {
-          console.log("App Data Source has been initialized!");
-          connected = true;
-        })
-        .catch((err) => {
-          console.error("Error during Data Source initialization", err);
-        });
-    });
-  }
+	// if not connected, then retry in loop
+	while (!connected) {
+		await new Promise((resolve) => setTimeout(resolve, 3000)).then(async () => {
+			await AppDataSource.initialize()
+				.then(() => {
+					console.log("App Data Source has been initialized!");
+					connected = true;
+				})
+				.catch((err) => {
+					console.error("Error during Data Source initialization", err);
+				});
+		});
+	}
 })();
 
 
