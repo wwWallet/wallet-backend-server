ff0b222ba83c5acce8d5124e075f1be91b3c8551
Emmanouil Koukoularis <koukoularisman@gmail.com>
2023-08-25 08:48:09 +0300

    wallet mock config fix

--- samples/wallet-mock/config.js
+++ samples/wallet-mock/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	walletBackendUrl: "http://127.0.0.1:8002",
+	walletBackendUrl: "http://wallet-backend-server:8002",
 	// trustedIssuerDID: "did:ebsi:zc6MhmU4NbKSAtAHx8XgpEW",
 	uoaTrustedIssuerDID: "did:ebsi:zpq1XFkNWgsGB6MuvJp21vA",
 	vidTrustedIssuerDID: "did:ebsi:zyhE5cJ7VVqYT4gZmoKadFt",
