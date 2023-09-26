9d314a7ea52c03ce06f6d4d7c6df8d2e6fcb436c
gregory1996 <grigoriskatrakazas@gmail.com>
2023-09-26 17:45:01 +0300

    keep only 500 status

--- src/routers/issuance.router.ts
+++ src/routers/issuance.router.ts
@@ -63,10 +63,6 @@ issuanceRouter.post('/handle/authorization/response', async (req, res) => {
 			res.send({});
 		}
 		else if (result.val === IssuanceErr.STATE_NOT_FOUND) {
-			res.status(404).send({});
-		} else if (result.err && result.val.action === "generateOpenid4vciProof") {
-			res.status(409).send(result.val);
-		} else {
 			res.status(500).send({});
 		}
 	}
