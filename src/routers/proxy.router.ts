3de3061ab2330c1ff029b76cf86f044e95b1c04f
gkatrakazas <grigoriskatrakazas@gmail.com>
2025-05-09 23:48:26 +0300

    feat: handle binary responses and forward all response headers

--- src/routers/proxy.router.ts
+++ src/routers/proxy.router.ts
@@ -5,20 +5,36 @@ const proxyRouter: Router = express.Router();
 proxyRouter.post('/', async (req, res) => {
 	const { headers, method, url, data } = req.body;
 	try {
+		const isBinaryRequest = /\.(png|jpe?g|gif|webp|bmp|tiff?|ico)(\?.*)?(#.*)?$/i.test(url);
 		console.log("URL = ", url)
 		const response = await axios({
 			url: url,
 			headers: headers,
 			method: method,
 			data: data,
+			...(isBinaryRequest && { responseType: 'arraybuffer' }),
 			maxRedirects: 0,
 		});
 
-		return res.status(200).send({
+		if (isBinaryRequest) {
+			// forward all response headers
+			for (const key in response.headers) {
+				if (Object.prototype.hasOwnProperty.call(response.headers, key)) {
+					const value = response.headers[key];
+					if (value !== undefined) {
+						res.setHeader(key, value as string);
+					}
+				}
+			}
+			return res.status(response.status).send(response.data);
+		}
+
+		// JSON or other text content
+		return res.status(response.status).send({
 			status: response.status,
 			headers: response.headers,
 			data: response.data,
-		})
+		});
 	}
 	catch (err) {
 		if (err.response && err.response.data) {
