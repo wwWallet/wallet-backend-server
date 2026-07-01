0d00311362bc60c7d4dd079b0423a4206753c860
konkourgr <konkour@gunet.gr>
2026-07-01 13:01:12 +0300

    Fix indentation

--- src/migrations/1782737022760-AddSyncableToWebauthnCredential.ts
+++ src/migrations/1782737022760-AddSyncableToWebauthnCredential.ts
@@ -1,15 +1,14 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class AddSyncableToWebauthnCredential1782737022760 implements MigrationInterface {
-  name = 'AddSyncableToWebauthnCredential1782737022760'
 
-  public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE \`webauthn_credential\` ADD \`backupEligibility\` BOOLEAN NOT NULL DEFAULT 0`);
-    await queryRunner.query(`ALTER TABLE \`webauthn_credential\` ADD \`backupState\` BOOLEAN NOT NULL DEFAULT 0`);
-  }
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(`ALTER TABLE \`webauthn_credential\` ADD \`backupEligibility\` BOOLEAN NOT NULL DEFAULT 0`);
+		await queryRunner.query(`ALTER TABLE \`webauthn_credential\` ADD \`backupState\` BOOLEAN NOT NULL DEFAULT 0`);
+	}
 
-  public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE \`webauthn_credential\` DROP COLUMN \`backupEligibility\``);
-    await queryRunner.query(`ALTER TABLE \`webauthn_credential\` DROP COLUMN \`backupState\``);
-  }
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(`ALTER TABLE \`webauthn_credential\` DROP COLUMN \`backupEligibility\``);
+		await queryRunner.query(`ALTER TABLE \`webauthn_credential\` DROP COLUMN \`backupState\``);
+	}
 }
